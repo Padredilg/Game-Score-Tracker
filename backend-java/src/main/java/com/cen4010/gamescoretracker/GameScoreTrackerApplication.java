06f43a1c50b282666dc8d7af95c235c61228220c
Luiz Padredi <padredilg@gmail.com>
2025-11-04 19:50:53 -0500

    First Stage of app, pre-functionality (develop into main) (#15)
    * Vas checked in.
    
    * lowered
    
    * updated gitignore and readme, and removed unnecessary folder
    
    * Feature/update gitignore (#7)
    
    Updated gitignore and added comments on readme
    
    * created version file for application
    
    * Feature/angular-21-setup - initial angular template for Game Score Tracker App (#10)
    
    * test commit
    * updated angular to v21 for signal forms
    * angular 21 migration
    * removed unused imports and ran prettier
    
    * Jleahy909 patch 1 (#11)
    
    * Merge 1 -- gitgnore (#8)
    
    Git ignore and readme comments
    
    * Update README.md
    
    ---------
    
    Co-authored-by: Luiz Padredi <padredilg@gmail.com>
    
    * Backend Initial Files (#14)
    
    * got initial setup to be able to send request through endpoint when running backend
    * updated readme with backend instructions
    
    ---------
    
    Co-authored-by: Vasyl Kravchuk <vasiliykravchuk@gmail.com>
    Co-authored-by: Alex Bendana <alxbendana@gmail.com>
    Co-authored-by: alexbendana <132596963+alexbendana@users.noreply.github.com>
    Co-authored-by: jleahy909 <jtlheat@gmail.com>

---
+++ backend-java/src/main/java/com/cen4010/gamescoretracker/GameScoreTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.cen4010.gamescoretracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GameScoreTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GameScoreTrackerApplication.class, args);
+	}
+
+}
