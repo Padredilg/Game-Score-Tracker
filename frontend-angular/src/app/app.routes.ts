dd49ff5ea31500bd0cb974e396f6e1ca1f369ef9
KravVas <vasiliykravchuk@gmail.com>
2025-11-22 22:16:46 -0500

    Create Account page is added, working loaclly, test it please

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -3,8 +3,11 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'login',
-    // ⬇️ exact path, no spaces, all lowercase
     loadComponent: () => import('./login/login').then(m => m.LoginComponent)
+  },
+  {
+    path: 'register',
+    loadComponent: () => import('./register/register').then(m => m.RegisterComponent)
   },
   { path: '', pathMatch: 'full', redirectTo: 'login' },
   { path: '**', redirectTo: 'login' }
